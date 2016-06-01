15b7d5fbd2c0489c1da84e5a0063927f87b80056
hanwenya <hvergelmir07@gmail.com>
2016-06-01 13:51:56 -0500

    简单的学生信息处理程序实现
    来源: POJ (Coursera声明：在POJ上完成的习题将不会计入Coursera的最后成绩。)
    
    注意： 总时间限制: 1000ms 内存限制: 65536kB
    
    描述
    
    在一个学生信息处理程序中，要求实现一个代表学生的类，并且所有成员变量都应该是私有的。
    
    （注：评测系统无法自动判断变量是否私有。我们会在结束之后统一对作业进行检查，请同学们严格按照题目要求完成，否则可能会影响作业成绩。）
    
    输入
    
    姓名，年龄，学号，第一学年平均成绩，第二学年平均成绩，第三学年平均成绩，第四学年平均成绩。
    
    其中姓名、学号为字符串，不含空格和逗号；年龄为正整数；成绩为非负整数。
    
    各部分内容之间均用单个英文逗号","隔开，无多余空格。
    
    输出
    
    一行，按顺序输出：姓名，年龄，学号，四年平均成绩（向下取整）。
    
    各部分内容之间均用单个英�

---
+++ student_information_hnadle.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+class student_information{
+private:
+    char name[200];
+    int age;
+    char num[200];
+    int first_avg;
+    int second_avg;
+    int thrid_avg;
+    int fourth_avg;
+    int avg_score;
+public:
+    void setStudent();
+    void getAvg();
+    void getStudent();
+};
+
+void student_information::setStudent(){
+    char c;
+    cin.getline(name,200,',');
+    cin>>age>>c;
+    cin.getline(num,200,',');
+    cin>>first_avg>>c>>second_avg>>c>>thrid_avg>>c>>fourth_avg;
+}
+
+void student_information::getAvg(){
+    avg_score = (first_avg+second_avg+thrid_avg+fourth_avg)/4;
+}
+void student_information::getStudent(){
+    cout<<name<<","<<age<<","<<num<<","<<avg_score<<endl;
+}
+
+int main(){
+    student_information Student;
+    Student.setStudent();
+    Student.getAvg();
+    Student.getStudent();
+    return 0;
+}
